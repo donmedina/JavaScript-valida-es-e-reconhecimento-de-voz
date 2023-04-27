207880f1dc00bc52a0108d1000e8c95ae545340d
Gabriel Medina <medina.rj@live.com>
2023-04-27 19:43:39 -0300

    Aula 3

---
+++ js/reconhecimentoVoz.js
@@ -0,0 +1,21 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(event){
+    const escolha = event.results[0][0].transcript;
+    // document.querySelector('.box').innerHTML = escolha;
+    exibeChute(escolha);
+}
+
+const elementoChute = document.getElementById('chute');
+function exibeChute(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    `
+}
