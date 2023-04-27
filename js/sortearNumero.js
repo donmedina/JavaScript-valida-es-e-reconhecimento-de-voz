54f2887f618921c4c7c61b391cd3e07318f60eee
Gabriel Medina <medina.rj@live.com>
2023-04-27 19:13:18 -0300

    Aula 2

---
+++ js/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumero();
+
+
+function gerarNumero(){
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+console.log('Numero sorteado:', numeroSecreto);
+
+const elementoMenor = document.getElementById('menor-valor');
+elementoMenor.innerHTML = menorValor;
+const elementoMaior = document.getElementById('maior-valor');
+elementoMaior.innerHTML = maiorValor;
